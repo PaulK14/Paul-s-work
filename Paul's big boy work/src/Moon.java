b0b96888a55348fc3f176ed43e64a074f3b9ca7c
PaulK14 <99052262+PaulK14@users.noreply.github.com>
2022-03-11 14:11:10 -0800

    fianl for big boy

---
+++ Paul's big boy work/src/Moon.java
@@ -0,0 +1,3 @@
+public class Moon {
+    private Planet
+}
---
+++ Paul's big boy work/src/Planet.java
@@ -0,0 +1,12 @@
+public class Planet {
+    private int orbitTime;
+    private Arraylist<Moon> Moons;
+    private String designation;
+    Planet(){
+
+    }
+        Planet(int orbitTime, String designation) {
+
+        }
+}
+
