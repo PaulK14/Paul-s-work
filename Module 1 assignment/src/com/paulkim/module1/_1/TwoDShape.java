62b65a0843f3dd5c7383bb724857963d154b3871
PaulK14 <99052262+PaulK14@users.noreply.github.com>
2022-04-07 14:48:37 -0700

    ree

---
+++ Module 1 assignment/src/com/paulkim/module1/_1/TwoDShape.java
@@ -0,0 +1,16 @@
+package com.paulkim.module1._1;
+
+public class TwoDShape {
+    double width;
+    double height;
+    public TwoDShape(double width, double height) {
+
+    }
+    public TwoDShape() {
+
+    }
+    public double getArea() {
+
+    }
+
+}
