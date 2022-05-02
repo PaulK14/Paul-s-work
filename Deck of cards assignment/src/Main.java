4e1e1e79f24a80cff6b2bc0b1d4b19dc2e0bde5c
PaulK14 <99052262+PaulK14@users.noreply.github.com>
2022-05-02 14:59:44 -0700

    badda bing deck of cards

---
+++ Deck of cards assignment/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+}
+/*make deck of cards with (face or number, numerical value, suit) ez pz right?.
+the difficult part is creating the actual values for the stuff.
+With loops you can make all the cards up to the ace? jokers? not sure but put in values for those I guess.
+the faces alone might be enough but the numerical values would help if you wanna do other stuff.
+onces you've made all the cards which I presume you have to do in separate classes, classifying everything.
+you can make code to make a deck with random cards. thenn that'll be it
+ */
+
+/* so for doing the above you'll have to look at the previous polymorphism and inheritance assignments
+to find out how to put in all the suits and stuff.
+Then look into using loops to make each thingy.
+Probably make some getters ans setters to be able to get the values and set the values as well.
+Once thats been done you need to make sure that you set the amount of cards there are ina deck.
+Cause inside a deck there are only so many cards
+After the deck build yourself a hand which is randomizing everything and giving yourself 5 cards
+Then you're done
+
+ */
