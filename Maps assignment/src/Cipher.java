5385c6895f394b7e2bb9f092f4868faa7d75e0a3
PaulK14 <99052262+PaulK14@users.noreply.github.com>
2022-05-26 15:03:36 -0700

    dont fail comsci :'*(

--- Maps assignment/src/Cipher.java
+++ Maps assignment/src/Cipher.java
@@ -1,8 +1,14 @@
+import java.util.HashMap;
+
 public class Cipher {
         String alphabit = "abcdefghijklmnopqrstuvwxyz";
         String[] alphaArray = alphabit.split("");
-        for(int i = 0; i < 27; i++) {
+        int shift = 5;
+        String Message = "pocket";
+        HashMap<Alphabetical, Order> Encrypt = new HashMap<Alphabetical, Order>();
         
+        for(int i = 0; i < 27; i++) {
+
         }
     }
 
