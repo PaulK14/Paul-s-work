b210012533c4ad3e06aa89b966d52a253dd63ed7
PaulK14 <99052262+PaulK14@users.noreply.github.com>
2022-06-10 15:03:56 -0700

    work

--- Fibonacci Sequence Assignment/src/Main.java
+++ Fibonacci Sequence Assignment/src/Main.java
@@ -1,8 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-
-
+public class main {
+    public static int main(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return main(n-1) + main(n - 2);
     }
-
 
 }
